d57c96a375cf9a138d55280063ab39566bca0d88
Henrique <lhenrique.dev@gmail.com>
2024-04-04 13:30:58 -0300

    feat: tela de historico

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,14 +1,19 @@
+import { HistoryDTO } from "@dtos/HistoryDTO";
 import { HStack, Heading, Text, VStack } from "native-base";
 
-export function HistoryCard() {
+type HistoryCardProps = {
+  data: HistoryDTO
+}
+
+export function HistoryCard({ data }: HistoryCardProps) {
   return (
     <HStack w="full" px={5} py={4} mb={3} bg="gray.600" rounded="md" alignItems="center" justifyContent="space-between">
       <VStack mr={5} flex={1}>
-        <Heading color="white" fontFamily="heading" fontSize="md" textTransform="capitalize" numberOfLines={1}>Costas</Heading>
-        <Text color="gray.100" fontSize="lg" numberOfLines={1}>Puxada frontal</Text>
+        <Heading color="white" fontFamily="heading" fontSize="md" textTransform="capitalize" numberOfLines={1}>{data.group}</Heading>
+        <Text color="gray.100" fontSize="lg" numberOfLines={1}>{data.name}</Text>
       </VStack>
 
-      <Text color="gray.300" fontSize="md">08:00</Text>
+      <Text color="gray.300" fontSize="md">{data.hour}</Text>
     </HStack>
   )
 }
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,20 +1,37 @@
-import { useState } from 'react';
-import { Heading, VStack, SectionList, Text } from "native-base";
+import { useCallback, useEffect, useState } from 'react';
+import { Heading, VStack, SectionList, Text, useToast } from "native-base";
 
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
+import { AppError } from '@utils/AppError';
+import { api } from '@services/api';
+import { useFocusEffect } from '@react-navigation/native';
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO';
 
 export function History() {
-  const [exercises, setExercises] = useState([
-    {
-      title: '26.08.2022',
-      data: ['Puxada frontal', 'Remada unilateral', 'Remada curvada', 'Levantamento terra']
-    },
-    {
-      title: '27.08.2022',
-      data: ['Puxada frontal', 'Remada unilateral']
+  const [isLoading, setIsLoading] = useState(true)
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([])
+
+  const toast = useToast()
+
+  const fetchHistory = async () => {
+    try {
+      setIsLoading(true)
+      
+      const { data } = await api.get('/history')
+      setExercises(data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError ? error.message : 'Não foi possível carregar o histórico. Tente mais tarde.'
+      toast.show({ title, placement: 'top', bgColor: 'red.500' })
+    } finally {
+      setIsLoading(false)
     }
-  ])
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchHistory()
+  }, []))
 
   return (
     <VStack flex={1}>
@@ -22,9 +39,9 @@ export function History() {
 
       <SectionList
         sections={exercises}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <HistoryCard />
+          <HistoryCard data={item}/>
         )}
         renderSectionHeader={({ section: { title } }) => (
           <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">{title}</Heading>
